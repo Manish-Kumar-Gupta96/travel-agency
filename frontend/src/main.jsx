a32be0a1cf6a93351baa7fe98874f94ae2be0119
Your Name <you@example.com>
2026-09-18 13:39:19 +0530

    Fix blank screen by switching to HashRouter for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,104 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import {
-    BrowserRouter
-} from "react-router-dom";
-
-
-import {
-    Provider
-} from "react-redux";
-
-
-import {
-    QueryClient,
-    QueryClientProvider
-} from "@tanstack/react-query";
-
-
-import {
-    HelmetProvider
-} from "react-helmet-async";
-
-
-import {
-    ToastContainer
-} from "react-toastify";
-
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
+import { ToastContainer } from "react-toastify";
 
 import App from "./App";
-
 import store from "./store";
 
-
 import "./assets/css/app.css";
-
-
 import "react-toastify/dist/ReactToastify.css";
 
-
-
 const queryClient = new QueryClient({
-
     defaultOptions: {
-
         queries: {
-
             retry: 1,
-
             refetchOnWindowFocus: false
-
         }
-
     }
-
 });
 
-
-
-ReactDOM.createRoot(
-    document.getElementById("root")
-)
-.render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-
-
         <Provider store={store}>
-
-
             <QueryClientProvider client={queryClient}>
-
-
                 <HelmetProvider>
-
-
-                    <BrowserRouter>
-
-
+                    <HashRouter>
                         <App />
-
-
-                    </BrowserRouter>
-
-
+                    </HashRouter>
                 </HelmetProvider>
-
-
             </QueryClientProvider>
-
-
         </Provider>
-
-
-
         <ToastContainer
             position="top-right"
             autoClose={3000}
         />
-
-
     </React.StrictMode>
-
 );
